a56f3651757f9078db6624974bb5f86ebaa7e333
malonetheressa <theressadmalone@gmail.com>
2024-06-21 16:52:51 +0200

    :truck: reorganise structure

--- front/babel.config.js
+++ front/babel.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  presets: [
-      '@babel/preset-env',
-      '@babel/preset-modules',
-      '@babel/preset-react',
-  ],
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-modules',
+        '@babel/preset-react',
+    ],
+    plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-proposal-unicode-property-regex',
+    ],
 };
