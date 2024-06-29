90afe2f9fbc7bca2e8b48a2a970604ae04af0ec5
malonetheressa <theressadmalone@gmail.com>
2024-06-29 18:13:39 +0200

    :iphone: implement mobile view for chat

--- front/src/components/footer/Footer.js
+++ front/src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="bg-primary py-8 px-4 sm:px-6 lg:px-8 text-center text-white">
+        <div className="fixed bottom-0 left-0 right-0 bg-primary py-8 px-4 sm:px-6 lg:px-8 text-center text-white">
             <p>&copy; 2024 GoodNeighbour 💗 All rights reserved.</p>
         </div>
     );
--- front/src/components/messages/MessageSender.js
+++ front/src/components/messages/MessageSender.js
@@ -1,56 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
-
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const MessageSender = ({ currChat, currUser, setRefreshMessages }) => {
+    const [messageContent, setMessageContent] = useState('');
 
-  const [messageContent, setMessageContent] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:3000/messages', {
-        message: {
-          chat_id: currChat.id,
-          user_id: currUser.id,
-          content: messageContent,
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(
+                'http://localhost:3000/messages',
+                {
+                    message: {
+                        chat_id: currChat.id,
+                        user_id: currUser.id,
+                        content: messageContent,
+                    },
+                }
+            );
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error sending message:', error);
         }
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
 
-    setRefreshMessages(true);
-  };
+        setRefreshMessages(true);
+        setMessageContent('');
+    };
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-      <h2 className="text-3xl font-semibold mb-6 text-center text-green-700">Send message</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="content" className="block text-sm font-medium text-gray-700">Content</label>
-          <textarea
-            id="content"
-            className="mt-1 p-2 w-full border border-gray-300 rounded-lg focus:outline-none focus:border-green-500"
-            value={messageContent}
-            onChange={(e) => setMessageContent(e.target.value)}
-            required
-          />
+    return (
+        <div className="flex flex-col pb-10 mb-10 items-center justify-end overflow-y-scroll ">
+            <div className="bg-light rounded-lg w-full max-w-md p-8">
+                <h2 className="text-2xl font-semibold mb-6 text-center text-primary">
+                    Send message
+                </h2>
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <label
+                            htmlFor="content"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Content
+                        </label>
+                        <textarea
+                            id="content"
+                            className="mt-1 p-3 w-full border border-gray-300 rounded-lg focus:outline-none focus:border-secondary"
+                            value={messageContent}
+                            onChange={(e) => setMessageContent(e.target.value)}
+                            required
+                            rows={4}
+                            placeholder="Type your message here..."
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full py-3 px-4 bg-secondary rounded-lg text-white hover:bg-dark focus:outline-none focus:bg-dark transition duration-200"
+                    >
+                        Send message
+                    </button>
+                </form>
+            </div>
         </div>
-
-        <button
-          type="submit"
-          className="w-full py-3 px-4 bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:bg-green-600 transition duration-200"
-        >
-          Send Message
-        </button>
-      </form>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MessageSender;
